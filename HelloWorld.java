7942bf85e3f84534c8c4ac035199a0ee169e3378
tomatenbrei <jannis.weil@web.de>
2014-10-09 22:52:46 +0200

    Deutsch hinzugefügt

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,9 @@
 public class HelloWorld {
 	public static void main (String[] args) {
+		//Deutsch
 		System.out.println("hallo");
 		System.out.println("Im Deutschen heisst es \"hallo\"");
+		//English
 		System.out.println("hello");
 	}
 }
